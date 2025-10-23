21dd1398f499605680ad892884f325db55b54e8b
Jacked-Son <166876544+Jacked-Son@users.noreply.github.com>
2025-10-23 12:02:46 -0500

    Delete include/option_plus_menu.h

--- include/option_plus_menu.h
+++
@@ -1,8 +0,0 @@
-#ifndef GUARD_OPTION_PLUS_MENU_H
-#define GUARD_OPTION_PLUS_MENU_H
-
-#define gText_OptionsMenuSave "Save"
-
-void CB2_InitOptionPlusMenu(void);
-
-#endif // GUARD_OPTION_PLUS_MENU_H
