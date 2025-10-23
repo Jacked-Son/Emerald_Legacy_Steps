12b705ceb6bf216a230896c4f828573eb4673038
Jacked-Son <166876544+Jacked-Son@users.noreply.github.com>
2025-10-23 10:55:54 -0500

    Update event_object_movement.c

--- src/event_object_movement.c
+++ src/event_object_movement.c
@@ -2127,7 +2127,7 @@ void UpdateFollowingPokemon(void) { // Update following pokemon if any
         !GetFollowerInfo(&species, &form, &shiny)
         || (gMapHeader.mapType == MAP_TYPE_INDOOR && SpeciesToGraphicsInfo(species, 0)->oam->size > ST_OAM_SIZE_2)
         || FlagGet(FLAG_TEMP_HIDE_FOLLOWER)
-        || !FlagGet(FLAG_ENABLE_FOLLOWER)
+        || !FlagGet(FLAG_HIDE_FOLLOWER)
         || gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_MACH_BIKE
         || gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_ACRO_BIKE
     )
