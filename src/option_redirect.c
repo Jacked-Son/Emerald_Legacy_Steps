534318ca58a0f93d70d0ddb97c841d6262c7c42d
Jacked-Son <166876544+Jacked-Son@users.noreply.github.com>
2025-10-23 09:33:17 -0500

    Add files via upload
    Critical File, redirects old menu to new menu from enhanced

---
+++ src/option_redirect.c
@@ -0,0 +1,8 @@
+#include "global.h"
+#include "option_plus_menu.h"
+
+// Replace all vanilla option entries with the new Option Plus screen.
+void CB2_InitOptionMenu(void)
+{
+    CB2_InitOptionPlusMenu();
+}
