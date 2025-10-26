b782d38766edcb8fd94790b590fd0f670716b797
Jacked-Son <166876544+Jacked-Son@users.noreply.github.com>
2025-10-26 11:19:20 -0500

    Update party_menu.c

--- src/party_menu.c
+++ src/party_menu.c
@@ -4983,9 +4983,8 @@ void ItemUseCB_RareCandy(u8 taskId, TaskFunc task)
     u16 *itemPtr = &gSpecialVar_ItemId;
     bool8 cannotUseEffect;
 
-    if (GetMonData(mon, MON_DATA_LEVEL) != MAX_LEVEL)
-    // && !levelCappedNuzlocke(GetMonData(mon, MON_DATA_LEVEL)))
-    && !(FlagGet(FLAG_NUZLOCKE) && GetMonData(mon, MON_DATA_DEAD))
+    if (GetMonData(mon, MON_DATA_LEVEL) != MAX_LEVEL
+    && !(FlagGet(FLAG_NUZLOCKE) && GetMonData(mon, MON_DATA_DEAD)))
     {
         BufferMonStatsToTaskData(mon, arrayPtr);
         cannotUseEffect = ExecuteTableBasedItemEffect_(gPartyMenu.slotId, *itemPtr, 0);
